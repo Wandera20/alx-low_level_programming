35e80d9ef60f89260de34644add12019c8e59370
Isaac Wandera <wanderaisaac20@gmail.com>
2023-03-16 13:59:35 -0700

    using puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h
+/**
+ * main -programing is like building a puzzle
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
