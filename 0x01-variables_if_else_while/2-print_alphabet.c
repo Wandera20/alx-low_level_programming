407d7cd3410bbb7536faf10930db8b0de2d97fa3
Isaac Wandera <wanderaisaac20@gmail.com>
2023-03-17 12:09:52 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
