3103c30dfd0278517b4b6ba481ab06ba88f90139
Isaac Wandera <wanderaisaac20@gmail.com>
2023-03-17 13:33:06 -0700

    printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing numbers using putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
