2a265e37667f65fa892dacd6e91a3603b4030272
Isaac Wandera <wanderaisaac20@gmail.com>
2023-03-21 13:18:18 -0700

    lower alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - printing lowercase alphabets
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	-putchar('\n');
+}
