9ab5c56181b198d3cab37572036bcb5f1fa9bff6
Isaac Wandera <wanderaisaac20@gmail.com>
2023-03-24 07:18:53 -0700

    returning multiple of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _mul -prints multiple of aand b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 (success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
