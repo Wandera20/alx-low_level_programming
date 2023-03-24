5c3c3c5bd5aabc7741c2c09ae6de92eeb271497a
Isaac Wandera <wanderaisaac20@gmail.com>
2023-03-24 08:59:31 -0700

    printing /

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws lines accordingly
+ * @n: number of lines to be drawn
+ * Return: empty
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
