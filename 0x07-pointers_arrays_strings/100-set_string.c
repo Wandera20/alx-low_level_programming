95701222fbc58b34405e92b3d5dc3d6741fa62c7
Isaac Wandera <wanderaisaac20@gmail.com>
2023-04-04 08:05:37 -0700

    signing a pointer to a character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets a pointer to a character
+ * @s: pointer to pointer
+ * @to: pointer to a character
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
