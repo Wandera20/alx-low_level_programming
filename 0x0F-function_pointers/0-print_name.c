492888145543d7b48b4aa41480999d395f21e67e
Isaac Wandera <wanderaisaac20@gmail.com>
2023-04-19 23:35:17 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  *print_name - function that prints a name
  *@name: name input
@@ -8,5 +9,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+	return;
+	}
 	(f(name));
 }
